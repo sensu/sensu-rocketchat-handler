b3a3c82097ba750e6cf8a3005bb6ca94d1ac974e
Jef Spaleta <jspaleta@gmail.com>
2021-06-23 21:48:03 -0800

    functioning status messages

--- main.go
+++ main.go
@@ -12,6 +12,9 @@ import (
 
 	"github.com/sensu-community/sensu-plugin-sdk/sensu"
 	"github.com/sensu/sensu-go/types"
+	"github.com/sensu/sensu-plugin-sdk/templates"
+
+	corev2 "github.com/sensu/sensu-go/api/core/v2"
 )
 
 type LoginResponse struct {
@@ -24,6 +27,12 @@ type LoginResponse struct {
 	}
 	Response string
 }
+type InfoResponse struct {
+	User struct {
+		Type string
+	}
+	Success bool
+}
 
 type PostMessageResponse struct {
 	Success bool
@@ -32,6 +41,35 @@ type PostMessageResponse struct {
 
 type LogoutResponse struct {
 	Status string
+}
+
+type MsgAttachment struct {
+	Color       string            `json:"color,omitempty"`
+	Text        string            `json:"text,omitempty"`
+	Ts          string            `json:"ts,omitempty"`
+	ThumbUrl    string            `json:"thumb_url,omitempty"`
+	MessageLink string            `json:"message_link,omitempty"`
+	AuthorName  string            `json:"author_name,omitempty"`
+	AuthorLink  string            `json:"author_link,omitempty"`
+	AuthorIcon  string            `json:"author_icon,omitempty"`
+	Title       string            `json:"title,omitempty"`
+	ImageUrl    string            `json:"image_url,omitempty"`
+	Fields      []AttachmentField `json:"fields,omitempty"`
+}
+
+type AttachmentField struct {
+	Short bool   `json:"short,omitempty"`
+	Title string `json:"title"`
+	Value string `json:"value"`
+}
+
+type RocketMsg struct {
+	Channel     string          `json:"channel"`
+	Text        string          `json:"text,omitempty"`
+	Alias       string          `json:"alias,omitempty"`
+	Emoji       string          `json:"emoji,omitempty"`
+	Avatar      string          `json:"avatar,omitempty"`
+	Attachments []MsgAttachment `json:"attachments,omitempty"`
 }
 
 // Config represents the handler plugin config.
@@ -46,6 +84,8 @@ type Config struct {
 	UserID              string
 	Channel             string
 	DescriptionTemplate string
+	Alias               string
+	Avatar              string
 }
 
 const (
@@ -146,6 +186,22 @@ var (
 			Usage:     "RocketChat Auth UserID. Used with --token. Note for security using ROCKETCHAT_USERID environment variable is preferred",
 			Value:     &plugin.UserID,
 		},
+		&sensu.PluginConfigOption{
+			Path:     "alias",
+			Env:      "ROCKETCHAT_ALIAS",
+			Argument: "alias",
+			Default:  "sensu",
+			Usage:    "Name to use in the RocketChat msg. (Note: user must have bot role to take effect)",
+			Value:    &plugin.Alias,
+		},
+		&sensu.PluginConfigOption{
+			Path:     "avatar-url",
+			Env:      "ROCKETCHAT_AVATAR_URL",
+			Argument: "avatar-url",
+			Default:  "https://www.sensu.io/img/sensu-logo.png",
+			Usage:    "Avatar image url to use in RocketChat msg. (Note: user must have bot role to take effect)",
+			Value:    &plugin.Avatar,
+		},
 	}
 )
 
@@ -187,8 +243,6 @@ func checkArgs(_ *types.Event) error {
 }
 
 func executeHandler(event *types.Event) error {
-	//Construct Msg from Event
-	message := "test msg"
 
 	// Login if not using token auth
 	loginData := LoginResponse{}
@@ -199,6 +253,11 @@ func executeHandler(event *types.Event) error {
 	}
 
 	if loginData.Status == "success" {
+		message, err := buildMsg(event)
+		//Construct Msg from Event
+		if err != nil {
+			fmt.Printf("%s: Error processing template: %s", plugin.PluginConfig.Name, err)
+		}
 		if plugin.DryRun {
 			log.Printf("DryRun Report:\n Channel: %v Server: %v\n Msg: %v",
 				plugin.Channel, plugin.Url, message)
@@ -227,6 +286,115 @@ func executeHandler(event *types.Event) error {
 		log.Fatal(loginData.Response)
 	}
 	return nil
+}
+
+func buildMsg(event *corev2.Event) (string, error) {
+	msg := RocketMsg{}
+	msg.Channel = plugin.Channel
+	if isBot() {
+		msg.Alias = plugin.Alias
+		msg.Avatar = plugin.Avatar
+	}
+	msg.Attachments = []MsgAttachment{
+		messageAttachment(event),
+	}
+	m, err := json.Marshal(msg)
+	return string(m), err
+}
+
+func isBot() bool {
+
+	u, err := url.Parse(plugin.Url)
+	u.Path = path.Join(u.Path, "/api/v1/users.info")
+	body := strings.NewReader(`userId=` + plugin.UserID)
+	req, err := http.NewRequest("GET", u.String(), body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.Header.Set("X-Auth-Token", plugin.Token)
+	req.Header.Set("X-User-Id", plugin.UserID)
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	if plugin.Verbose {
+		log.Printf("User Info Http Request: %+v", req)
+	}
+	infoResponse := InfoResponse{}
+	if plugin.DryRun {
+		infoResponse.Success = true
+		infoResponse.User.Type = "dry-run"
+	} else {
+
+		resp, err := http.DefaultClient.Do(req)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer resp.Body.Close()
+		b, _ := ioutil.ReadAll(resp.Body)
+		err = json.Unmarshal(b, &infoResponse)
+		if plugin.Verbose {
+			fmt.Printf("User Info Response: Bot: %v\n", infoResponse.User.Type == `bot`)
+
+		}
+	}
+	if infoResponse.Success {
+		return infoResponse.User.Type == "bot"
+	} else {
+		return false
+	}
+
+}
+func messageAttachment(event *corev2.Event) MsgAttachment {
+	description, err := templates.EvalTemplate("description", plugin.DescriptionTemplate, event)
+	if err != nil {
+		log.Printf("%s: Error processing template: %s", plugin.PluginConfig.Name, err)
+	}
+	description = strings.Replace(description, `\n`, "\n", -1)
+
+	a := MsgAttachment{
+		Title: "Description",
+		Color: messageColor(event),
+	}
+	fields := []AttachmentField{
+		{
+			Title: "Status",
+			Value: messageStatus(event),
+			Short: false,
+		},
+		{
+			Title: "Entity",
+			Value: event.Entity.Name,
+			Short: true,
+		},
+		{
+			Title: "Check",
+			Value: event.Check.Name,
+			Short: true,
+		},
+	}
+	a.Fields = fields
+	return a
+}
+
+func messageStatus(event *corev2.Event) string {
+	switch event.Check.Status {
+	case 0:
+		return "Resolved"
+	case 2:
+		return "Critical"
+	default:
+		return "Warning"
+	}
+}
+
+func messageColor(event *corev2.Event) string {
+	switch event.Check.Status {
+	case 0:
+		return "green"
+	case 2:
+		return "red"
+	default:
+		return "orange"
+	}
 }
 
 func login() LoginResponse {
@@ -266,14 +434,14 @@ func login() LoginResponse {
 func postMessage(message string) PostMessageResponse {
 	u, err := url.Parse(plugin.Url)
 	u.Path = path.Join(u.Path, "/api/v1/chat.postMessage")
-	body := strings.NewReader(`channel=#` + plugin.Channel + `&text=` + message)
+	body := strings.NewReader(message)
 	req, err := http.NewRequest("POST", u.String(), body)
 	if err != nil {
 		log.Fatal(err)
 	}
 	req.Header.Set("X-Auth-Token", plugin.Token)
 	req.Header.Set("X-User-Id", plugin.UserID)
-	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Set("Content-Type", "application/json")
 	if plugin.Verbose {
 		log.Printf("PostMessage Http Request: %+v", req)
 	}
